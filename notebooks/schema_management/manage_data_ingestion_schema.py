5c3ca709112755643bdea32a585d4eb92aafcd5f
Stefan Bojarovski <stefan.bojarovski@zalando.de>
2026-04-04 15:49:46 +0200

    Create a metadata management notebook and job for the ingestion data objects

---
+++ notebooks/schema_management/manage_data_ingestion_schema.py
@@ -0,0 +1,68 @@
+# Databricks notebook source
+import logging
+
+from llmops_databricks_course_sbojarovski.config import settings
+from llmops_databricks_course_sbojarovski.providers import provide_spark_session
+
+# COMMAND ----------
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
+
+# COMMAND ----------
+spark = provide_spark_session()
+
+# COMMAND ----------
+# Set catalog
+logger.info(f"Set catalog {settings.CATALOG_NAME}")
+spark.catalog.setCurrentCatalog(settings.CATALOG_NAME)
+
+# COMMAND ----------
+
+logger.info(f"Creating schema '{settings.DATA_INGESTION_SCHEMA_NAME}'...")
+spark.sql(f"""
+        CREATE SCHEMA IF NOT EXISTS {settings.DATA_INGESTION_SCHEMA_NAME}
+        WITH DBPROPERTIES (
+        Name='{settings.USER_SHORT_NAME}',
+        Environment='{settings.ENV}'
+        )
+        """)
+logger.info(f"Successfully created schema '{settings.DATA_INGESTION_SCHEMA_NAME}'")
+
+# COMMAND ----------
+logger.info(f"Updating schema '{settings.DATA_INGESTION_SCHEMA_NAME}'...")
+spark.sql(f"""
+        COMMENT ON SCHEMA {settings.DATA_INGESTION_SCHEMA_NAME} IS
+        'This schema contains objects where ingested data is stored for the llm agent RAG'
+        """)
+
+spark.sql(f"""
+        ALTER SCHEMA {settings.DATA_INGESTION_SCHEMA_NAME}
+                SET TAGS (
+                'environment' = '{settings.ENV}',
+                'owner_name' = '{settings.USER_SHORT_NAME}'
+                )
+        """)
+logger.info(f"Successfully updated schema '{settings.DATA_INGESTION_SCHEMA_NAME}'")
+spark.catalog.setCurrentDatabase(settings.DATA_INGESTION_SCHEMA_NAME)
+
+# COMMAND ----------
+logger.info(f"Creating {settings.PDF_STORED_METADATA_TABLE_NAME} table...")
+spark.sql(f"""
+    CREATE TABLE IF NOT EXISTS {settings.PDF_STORED_METADATA_TABLE_NAME} (
+        paper_id STRING COMMENT 'The id of the paper in arxiv',
+        title STRING COMMENT 'The title of the paper',
+        authors ARRAY<STRING> COMMENT 'The authors of the paper',
+        summary STRING COMMENT 'The summary of the paper',
+        published_datetime LONG 'The published timestamp of the paper',
+        processed_datetime LONG 'The timestamp when the paper was processed by the ingestion pipeline',
+        volume_path STRING 'The path in the volume where the paper is stored'
+    )
+    USING DELTA
+""")
+spark.sql(f"""
+    COMMENT ON TABLE {settings.PDF_STORED_METADATA_TABLE_NAME} IS
+    'This table contains metadata of the papers whose PDFs have been ingested and stored in the volume. It serves as a checkpoint for incremental ingestion.'
+""")
+logger.info(f"Successfully created {settings.PDF_STORED_METADATA_TABLE_NAME} table")
